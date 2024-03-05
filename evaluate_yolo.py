0cf1827abb0b4c537dd0a26ca281ff986ab0695a
Java Terry <33javalava@gmail.com>
2024-03-05 13:43:04 +1000

    yolo blob detection start

---
+++ evaluate_yolo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env/python3
+
+"""Script to numerically evaluatte the performance of a YOLO model"""
+
+import os
+import glob
+import cv2 as cv
+import numpy as np
+import math
+import machinevisiontoolbox as mvt
+from machinevisiontoolbox.Image import Image as MvtImage
+import matplotlib.pyplot as plt
+
+weights = 
+
